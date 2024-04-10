dff640c4ed74ecb00e19e75ba794ca001e78f1d6
Your Name <you@example.com>
2024-04-10 10:23:48 +0300

    pd

--- main.py
+++ main.py
@@ -28,4 +28,68 @@
 # Saraksti - https://www.w3schools.com/python/python_lists.asp
 # Vārdnīcas - https://www.w3schools.com/python/python_dictionaries.asp
 # Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
-#
+
+
+import json
+
+per_ces = []
+# read movies file into variable
+with open('kase.json', 'r') as openfile:
+    # Reading from json file
+    per_ces = json.load(openfile)
+
+
+    def top(per_ces): 
+        return int(per_ces["cena"])
+
+
+while True: #dod izvēlēties ko darīt
+    print("\nkases aparāts:")
+    print("1. pivienot preci")
+    print("2. dzēst preci")
+    print("3. dzēst sarakstu")
+    print("4. samaksāt")
+    print("5. iziet")
+    choice = input("Enter your choice: ")
+
+    if choice == "1": # var pivirnot preci 
+        preces_nosaukums = input("ievadiet nosukumu: ")
+        cena = input("ievadiet cenu: ")
+        prece = {'prece': preces_nosaukums , 'cena': cena }
+        per_ces.append(prece)
+        pass
+    elif choice == "2": # dod izzdzēst preci
+        id = int(input("izvelēties preces numuru: "))
+        per_ces.pop(id)
+        pass
+    elif choice == "3": # izdzēš visas preces
+        del per_ces
+        print("saraksts izdzēsts")
+        pass
+    elif choice == "4":# meiģina samaksāt
+        per_ces.sort(key = top)
+        print(per_ces)
+        summa = input("ievadiet summu")
+        nauda1 = prece
+        print(nauda1)
+
+
+
+
+        if summa == nauda1 :
+            print("viss ok")
+        if summa < nauda1 :
+            print("nav pietiekami naudas")
+        if summa > nauda1 :
+            print("naudas par daudz, bet mēs to paņemsim sev")
+
+        print(
+            "čeks summa ir ", nauda1,"samaksāts ir ", summa 
+        )
+
+        pass
+    elif choice == "5": #iziet no programas 
+        print("Exiting...")
+        break
+    with open("json", "w") as pukupki: # saglabā produktus
+        json.dump(per_ces, pukupki)
